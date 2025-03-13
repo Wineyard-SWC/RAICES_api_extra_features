267fa929ff29589e8455c04c361aac693efdb378
Abdiel <131422484+AbdielFritsche@users.noreply.github.com>
2025-03-12 23:10:18 -0600

    Setup New Branch

--- Backend/models/__init__.py
+++
@@ -1,3 +0,0 @@
-#La carpeta de models es para crear las 'Clases' que representarian la data que recibimos o enviamos
-from .ai_conversation_models import RequestBody, ChatMessage, ChatResponse, AddContentRequest
-from .ai_requirement_model import RequirementResponse
